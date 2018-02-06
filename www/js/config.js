d9bb27a0da899f8998fe102103f028d41c39ef6d
carterax <ikenna750@gmail.com>
2018-02-06 18:40:04 +0100

    new commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"http://zoulak.com/mobileapp/api",	
+	'DialogDefaultTitle':"zoulak",	
+	'pushNotificationSenderid':"600405925358",	
+	'facebookAppId':"145103032829379", 
+	'APIHasKey':"998022d71919ae44c84635659bd0354c"
+};
